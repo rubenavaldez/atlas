461e8cb50342ffdb59a9477db67dadd7c0b49328
rubenavaldez <rubenavaldez@yahoo.com>
2024-09-11 18:11:39 -0700

    added url imports

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,11 +1,37 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from rest_framework import generics
-from .serializers import UserSerializer
+from .serializers import UserSerializer, NoteSerializer
 from rest_framework.permissions import IsAuthenticated, AllowAny
+from .models import Note
 
 # Create your views here.
+
+class NoteListCreate(generics.ListCreateAPIView):
+    serializer_class = NoteSerializer
+    permission_class = [IsAuthenticated] # only an authenticated user can make a note
+
+    def get_queryset(self): # get query allows acces to user info, override the gerquery set, see django docs
+       user = self.request.user
+       return Note.objects.filter(author=user) # get notes associated with the user
+    
+    def perform_create(self,serializer): # overridding the create method, see django docs
+        if serializer.is_valid():
+            serializer.save(author=self.request.user) #maunally add an author
+        else:
+            print(serializer.error)
+
+class NoteDelete(generics.DestroyAPIView):
+    serializer_class = NoteSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self): # get query allows acces to user info, override the gerquery set, see django docs
+       user = self.request.user
+       return Note.objects.filter(author=user) # get notes associated with the user
+
+
+
 class CreateUserView(generics.CreateAPIView):
     queryset = User.objects.all()
-    serializer_class = User
+    serializer_class = UserSerializer
     permission_classes = [AllowAny] # allow non authenticated users to create a new user
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -3,10 +3,12 @@ from django.urls import path, include
 from api.views import CreateUserView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView  #import access and refresh tokens
 
+#all auth routes and imports
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/user/register/', CreateUserView.as_view(), name='register'),
     path('api/token', TokenObtainPairView.as_view(), name ='get_token'),
     path('api/refresh', TokenRefreshView.as_view(), name ='refresh'),
-    path('api-auth/', include('rest_framework.urls'))
+    path('api-auth/', include('rest_framework.urls')),
+    path("api/", include("api.urls")) #import urls from api url
 ]
